b43d07cd834a37fb191e3cf1a345a6d28d8e8fbe
iamjpsonkar <iamjpsonkar@gmail.com>
2019-05-13 00:57:37 +0530

    Remove duplicated directory

--- myenv/bin/vba_extract.py
+++
@@ -1,4 +0,0 @@
-#!/workspace/Youtube_trending/venv/bin/python3
-# EASY-INSTALL-SCRIPT: 'XlsxWriter==1.1.8','vba_extract.py'
-__requires__ = 'XlsxWriter==1.1.8'
-__import__('pkg_resources').run_script('XlsxWriter==1.1.8', 'vba_extract.py')
